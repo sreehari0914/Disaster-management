962417a2b53f98dcb759a4abd95c1cc1dc4862a6
sreehari0914 <sreehari0914@gmail.com>
2023-11-17 22:46:43 +0530

    16th nov commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,57 @@
-import React from 'react';
-
+import { TypeAnimation } from 'react-type-animation';
+import { Container, Row, Col } from 'react-bootstrap';
+import aid from '../images/Humanitarian Help-cuate (1).png'
 const LandingPage = () => {
-  const landingPageStyle = {
-    backgroundImage: 'url("https://images.unsplash.com/photo-1475776408506-9a5371e7a068?q=80&w=1958&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: 'white',
-    textAlign: 'center',
-    padding: '20px',
-  };
-
   return (
-    <div style={landingPageStyle}>
-      <h1>Welcome to Aid Connect</h1>
-      <p>
-        A platform where you can post your disaster needs and connect with people willing to help.
-      </p>
-    </div>
+    <Container fluid style={{ backgroundColor: '#2b3035', minHeight: '100vh' }}>
+      <Row className="align-items-center">
+        <Col md={6} style={{ paddingRight: '20px' }}>
+          <div style={{ paddingLeft: '30px', paddingTop: '50px' }}>
+            <h1 style={{ fontSize: '5em', color: '#fff', fontFamily: 'Poppins' }}>
+              Welcome  
+            </h1>
+            <h1 style={{ fontSize: '5em', color: '#fff', fontFamily: 'Poppins' }}>
+              to  
+            </h1>
+            <h1 style={{ fontSize: '5em', color: '#fff', fontFamily: 'Poppins' }}>
+               <span style={{ color: '#5c79e0' }}>Aid Connect</span>
+            </h1>
+            <div style={{ fontSize: '1.5em', color: '#fff', width: '100%' }}>
+            <Col md={0}>
+          <div style={{ paddingTop: '50px' }}>
+            <h2 style={{ color: '#5c79e0', fontFamily: 'Poppins' }}>
+              <TypeAnimation
+                sequence={[
+                  'Aid Connect-Connecting People During Disaster',
+                  1000,
+                  'Aid Connect-Facilitating Aid & Assistance',
+                  1000,
+                  'Aid Connect-Uniting Communities Together',
+                  1000,
+                  'Aid Connect-Helping Hands Together',
+                  1000
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
+            </h2>
+          </div>
+        </Col>
+             
+            </div>
+          </div>
+        </Col>
+        <Col md={6}>
+          <img
+            src={aid}
+            alt="Illustration"
+            style={{ width: '80%', height: 'auto' }}
+          />
+        </Col>
+        
+      </Row>
+    </Container>
   );
 };
 
